94e02c04c361864b8254c8580a734b2e69661578
PhilippeDataScienc <philippe.acquier@gmail.com>
2024-02-12 16:03:40 +0100

    finalized the 3rd terst

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -22,6 +22,10 @@ class TestNerClient(unittest.TestCase):
         self.assertIsInstance(ents, dict)
 
     def text_get_ents_given_spacy_PERSON_is_returned_serializes_to_Person(self):
-        pass
-
-
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'Laurent Fressinet', 'label_': 'PERSON'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {'ents': [{'ent': 'Laurent Fressinet', 'label': 'Person'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
