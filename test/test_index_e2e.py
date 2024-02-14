3877fffb4e727e24a2b0325caad449227ff2c7bd
Philippe ACQUIER <pacquier@fmas.local>
2024-02-14 10:18:59 +0100

    implemented some E2E tests, created index.html

--- test/test_index_e2e.py
+++ test/test_index_e2e.py
@@ -1,13 +1,23 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class E2ETests(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(r"C:\\Users\\pacquier\\PycharmProjects\\chromedriver_win32\\chromedriver.exe")
+        self.driver = webdriver.Firefox()
         self.driver.get('http://localhost:5000')
 
     def tearDown(self):
         self.driver.quit()
 
+    def test_browser_title_contains_app_name(self):
+        self.assertIn('Named Entity', self.driver.title)
+
+    def test_page_heading_is_named_entity_finder(self):
+        heading = self._find("heading").text
+        self.assertEqual('Named Entity Finder', heading)
+
+    def _find(self,val):
+        return self.driver.find_element(By.CSS_SELECTOR, f'[data-test-id="{val}"]')
